c9162d1237cc633977780f7705a17c35a60eabc2
SuxrobGM <suxrobGM@gmail.com>
2018-12-23 00:50:34 +0500

    Changed database table names

--- EC_WebSite/Models/ApplicationDbContext.cs
+++ EC_WebSite/Models/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -36,6 +37,15 @@ namespace EC_WebSite.Models
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // Changed standart identity table names
+            builder.Entity<User>(entity => { entity.ToTable(name: "Users"); });
+            builder.Entity<UserRole>(entity => { entity.ToTable(name: "Roles"); });
+            builder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("UserRoles"); });
+            builder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("UserClaims"); });
+            builder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("UserLogins"); });
+            builder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("UserToken"); });
+            builder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("RoleClaims"); });
 
             builder.Entity<ForumHeader>(entity =>
             {
