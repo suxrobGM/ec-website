fe8c570ae083b67ba153a3bd25e0989814bef204
SuxrobGM <suxrobGM@gmail.com>
2019-12-22 01:35:58 +0800

    added class SDK for VkDonate API

---
+++ src/VkDonate.Api/DonateClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VkDonate.Api
+{
+    public class DonateClient
+    {
+        private readonly string _apiKey;
+        private readonly string _baseAddress;
+        private readonly HttpClient _client;
+
+        public DonateClient(string apiKey)
+        {
+            _client = new HttpClient();
+            _apiKey = apiKey;
+            _baseAddress = "https://api.vkdonate.ru";
+        }
+
+        public async Task<Donation[]> GetDonationsAsync(ushort count = 10, ushort offset = 0, Sort sortBy = Sort.Date, Order orderBy = Order.Descending)
+        {
+            if (count > 50)
+                count = 50;
+
+            var requestUrl = new Uri($"{_baseAddress}?action=donates&count={count}&sort={GetSort(sortBy)}&order={GetOrder(orderBy)}&offset={offset}&key={_apiKey}");
+            var response = await _client.GetAsync(requestUrl);
+            var content = await response.Content.ReadAsStringAsync();
+            var donatesJson = JObject.Parse(content).GetValue("donates").ToString();
+            var donates = JsonConvert.DeserializeObject<Donation[]>(donatesJson);
+            return donates;
+        }
+
+        private string GetSort(Sort sort)
+        {
+            switch (sort)
+            {
+                case Sort.Date:
+                    return "date";
+                case Sort.Sum:
+                    return "sum";
+                default:
+                    return "date";
+            }
+        }
+
+        private string GetOrder(Order order)
+        {
+            switch (order)
+            {
+                case Order.Ascending:
+                    return "asc";
+                case Order.Descending:
+                    return "desc";
+                default:
+                    return "desc";
+            }
+        }
+    }
+}
---
+++ src/VkDonate.Api/Donation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VkDonate.Api
+{
+    public class Donation
+    {
+        public ulong Id { get; set; }
+        public ulong Uid { get; set; }
+        public DateTime Date { get; set; }
+        public ulong Sum { get; set; }
+        public string Msg { get; set; }
+        public bool Anon { get; set; }
+        public bool Visible { get; set; }
+    }
+
+    public enum Sort
+    {
+        Date,
+        Sum
+    }
+
+    public enum Order
+    {
+        Ascending,
+        Descending
+    }
+}
