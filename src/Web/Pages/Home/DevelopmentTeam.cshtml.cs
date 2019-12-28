afce78aac3ed42156be1055f16299a9c7faa5404
SuxrobGM <suxrobGM@gmail.com>
2019-12-29 01:04:40 +0800

    added about page

--- src/Web/Pages/Home/DevelopmentTeam.cshtml.cs
+++ src/Web/Pages/Home/DevelopmentTeam.cshtml.cs
@@ -1,17 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EC_Website.Data;
+using EC_Website.Models.UserModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace EC_Website.Pages.Home
 {
     public class DevelopmentTeamModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Models.UserModel.User> _userManager;
 
+        public DevelopmentTeamModel(ApplicationDbContext context, UserManager<Models.UserModel.User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public List<Models.UserModel.User> Developers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var devRole = await _context.Roles.FirstAsync(i => i.Role == Role.Developer);
+            var developersUserIds = _context.UserRoles.Where(i => i.RoleId == devRole.Id).Select(i => i.UserId);
+            Developers = await _context.Users.Where(i => developersUserIds.Contains(i.Id)).ToListAsync();
+        }
+
+        public async Task<IEnumerable<string>> GetUserRolesAsync(Models.UserModel.User user)
+        {
+            return await _userManager.GetRolesAsync(user);
         }
     }
 }
