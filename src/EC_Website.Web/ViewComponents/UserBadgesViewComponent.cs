e9e1d85c76c9c751b463b877405e362122388bd9
SuxrobGM <suxrobGM@gmail.com>
2020-07-08 01:00:46 +0800

    added user roles and badges view component

---
+++ src/EC_Website.Web/ViewComponents/UserBadgesViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using EC_Website.Core.Entities.UserModel;
+
+namespace EC_Website.Web.ViewComponents
+{
+    public class UserBadgesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ICollection<UserBadge> badges)
+        {
+            return View(badges);
+        }
+    }
+}
---
+++ src/EC_Website.Web/ViewComponents/UserRolesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using EC_Website.Core.Entities.UserModel;
+
+namespace EC_Website.Web.ViewComponents
+{
+    public class UserRolesViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserRolesViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ApplicationUser user)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            return View(roles);
+        }
+    }
+}
