14b61b96570ddb5c8cb8935fcebff2887b7fcdf8
SuxrobGM <suxrobGM@gmail.com>
2020-07-12 19:16:03 +0800

    added wiki search page

---
+++ src/EC_Website.Web/Pages/Wiki/Search.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuxrobGM.Sdk.AspNetCore.Pagination;
+using EC_Website.Core.Entities.WikiModel;
+using EC_Website.Core.Interfaces;
+
+namespace EC_Website.Web.Pages.Wiki
+{
+    public class SearchModel : PageModel
+    {
+        private readonly IRepository _repository;
+
+        public SearchModel(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public PaginatedList<WikiPage> WikiPages;
+
+        public IActionResult OnGet(string searchString, int pageIndex = 1)
+        {
+            var wikiPages = _repository.GetAll<WikiPage>();
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                wikiPages = wikiPages.Where(i => i.Content.Contains(searchString));
+            }
+
+            WikiPages = PaginatedList<WikiPage>.Create(wikiPages, pageIndex, 20);
+            return Page();
+        }
+    }
+}
